6591d4ca1bbdd32b8030eafe878fd8f166c5cce5
pyropecs <praveengopu0105@gmail.com>
2024-09-06 10:47:31 +0530

    scanner input initial

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import School.*;
 
 
+
+
 public class Main {
     public static void main(String[] args) {
         //School student is created
-        Student student = new Student("Praveen","3rd street,school avenue",13,
+
+        Student student = new Student("praveen","3rd street,school avenue",13,
                 'M',2002,"10th std");
         //School teacher is created
         Teacher teacher = new Teacher("Nithya","4th street,school avenue",
@@ -12,33 +15,35 @@ public class Main {
         //School driver is created
         Driver driver = new Driver("Ashok","1st street,raja nagar",
                 50,'M',10067,20);
-        //Luxury Bus is created
-        LuxuryBus luxuryBus = new LuxuryBus(2002,50);
-        //Normal Bus is created
-        NormalBus normalBus = new NormalBus(3001,50);
-        //Details of the Luxury Bus
-        luxuryBus.showBusDetails();
-        //Details of the Normal Bus
-        normalBus.showBusDetails();
-        //To enable air Conditioning
-        luxuryBus.enableAirConditioning();
-        //Assigning driver to luxury bus
-        luxuryBus.assign(driver);
-        //Assigning eligible passenger to luxury bus
-        luxuryBus.assign(teacher);
-        //Dropping off passenger from eligible bus
-        luxuryBus.dropPerson(teacher);
-        //Assigning ineligible passenger to a luxury bus
-        luxuryBus.assign(student);
-        //Assigning passenger to a normal bus
-        normalBus.assign(teacher);
-        //Assigning a passenger to a normal bus
-        normalBus.assign(student);
-        //Assigning a passenger to luxury bus, but they are already in normal bus
-        luxuryBus.assign(teacher);
-        //Disable air conditioning
-        luxuryBus.disableAirConditioning();
-        //Showing luxury Bus without Air condition
-        luxuryBus.showBusDetails();
+
+
+//        //Luxury Bus is created
+//        LuxuryBus luxuryBus = new LuxuryBus(2002,50);
+//        //Normal Bus is created
+//        NormalBus normalBus = new NormalBus(3001,50);
+//        //Details of the Luxury Bus
+//        luxuryBus.showBusDetails();
+//        //Details of the Normal Bus
+//        normalBus.showBusDetails();
+//        //To enable air Conditioning
+//        luxuryBus.enableAirConditioning();
+//        //Assigning driver to luxury bus
+//        luxuryBus.assign(driver);
+//        //Assigning eligible passenger to luxury bus
+//        luxuryBus.assign(teacher);
+//        //Dropping off passenger from eligible bus
+//        luxuryBus.dropPerson(teacher);
+//        //Assigning ineligible passenger to a luxury bus
+//        luxuryBus.assign(student);
+//        //Assigning passenger to a normal bus
+//        normalBus.assign(teacher);
+//        //Assigning a passenger to a normal bus
+//        normalBus.assign(student);
+//        //Assigning a passenger to luxury bus, but they are already in normal bus
+//        luxuryBus.assign(teacher);
+//        //Disable air conditioning
+//        luxuryBus.disableAirConditioning();
+//        //Showing luxury Bus without Air condition
+//        luxuryBus.showBusDetails();
     }
 }
